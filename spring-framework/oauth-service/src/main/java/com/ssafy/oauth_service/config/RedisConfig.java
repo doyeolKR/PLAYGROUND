35e6b78bd98db90ebca4dce42216cee66b8eb75f
hoan123432 <hoan123432@naver.com>
2023-03-02 23:07:03 +0900

    redis 관련 설정 on cloud server

--- spring-framework/oauth-service/src/main/java/com/ssafy/oauth_service/config/RedisConfig.java
+++ spring-framework/oauth-service/src/main/java/com/ssafy/oauth_service/config/RedisConfig.java
@@ -1,12 +1,17 @@
 package com.ssafy.oauth_service.config;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -20,13 +25,43 @@ public class RedisConfig {
     @Value("${spring.data.redis.password}")
     private String password;
 
+    @Value("${spring.redis.lettuce.pool.max-idle:10}")
+    private int maxIdle;
+
+    @Value("${spring.redis.lettuce.pool.max-total:10}")
+    private int maxTotal;
+
+    @Value("${spring.redis.lettuce.pool.min-idle:5}")
+    private int minIdle;
+
+    @Value("${spring.redis.lettuce.pool.time-between-eviction-runs:10}")
+    private int timeBetweenEvictionRuns;
+
+    @Value("${spring.redis.lettuce.pool.commandTimeout:5}")
+    private long commandTimeout;
+
+    @Value("${spring.redis.timeout:60}")
+    private int  redisTimeOut;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+
         redisStandaloneConfiguration.setHostName(host);
         redisStandaloneConfiguration.setPort(port);
         redisStandaloneConfiguration.setPassword(password);
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+
+        genericObjectPoolConfig.setMaxIdle(maxIdle);
+        genericObjectPoolConfig.setMaxTotal(maxTotal);
+        genericObjectPoolConfig.setMinIdle(minIdle);
+//        genericObjectPoolConfig.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRuns);
+
+        LettuceClientConfiguration lettuceClientConfiguration =
+                LettucePoolingClientConfiguration.builder().poolConfig(genericObjectPoolConfig)
+                        .commandTimeout(Duration.ofSeconds(commandTimeout)).build();
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, lettuceClientConfiguration);
     }
 
     @Bean
